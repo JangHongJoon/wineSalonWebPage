68278645f342fcf0b0274324ed81f87d2665b033
JangHongJoon <wkdghdwns19969@gmail.com>
2023-04-16 01:18:33 +0900

    Feat : Added for summary at the sharing platform

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head />
         <meta property="og:title" content="Wine Salon" />
         <meta property="og:description" content="와인 살롱" />
-        <meta property="og:image" content="https://d1fdloi71mui9q.cloudfront.net/k57NiMjCRzaJjbmswPMX_PYxL7KLT9amE9KmC" />
+        <meta property="og:image" content="/public/wine_salon_logo.png" />
         <meta property="og:image:width" content="100px" />
         <meta property="og:image:height" content="100px" />
         <link rel="icon" href="/favicon.ico" />
