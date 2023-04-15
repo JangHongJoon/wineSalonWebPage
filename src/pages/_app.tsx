dabb2268760c87fe5e2079bd533f41f0c77d1719
JangHongJoon <wkdghdwns19969@gmail.com>
2023-04-16 00:43:05 +0900

    Fix : Changed for tab title and icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/Head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Head>
+          <title>Wine Salon</title>
+        </Head>
+        <Component {...pageProps} />
+      </>
+  )
 }
